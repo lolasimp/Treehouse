bc330f797d98dfc14ca3d52c9ebb5070b403de2d
Lola Simpson <lolalsimp@gmail.com>
2018-04-06 23:51:30 -0500

    added domString

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,16 @@
 console.log('Lola');
 
 // PrintToDom
-// 
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
+} 
+
+const buildDomString = (data) => {
+    let domString = '';
+        domString += `<div> class="card">`;
+        domString +=    `<img src="${data.gravatar_url}">`;
+        domString +=    `<p id="points">${data.points.total}</p>`;
+        domString += `<div>`;
+        printToDom(domString, "bothPlayers-container");
+};
+
