15f0150304a764b83d42178e20b588f4f925640c
Lola Simpson <lolalsimp@gmail.com>
2018-04-08 15:44:58 -0500

    added winner and some styling, YASSSSSS

--- javascript/main.js
+++ javascript/main.js
@@ -7,40 +7,72 @@ const printToDom = (domString, divId) => {
 
 const buildDomString = (data1, data2) => {
     let domString = '';
-        domString += `<div> class="card">`;
+        domString += `<div class="card">`;
         domString +=    `<div class='col-md-4 col-md-offset-1'>`;
         domString +=        `<img src="${data1.gravatar_url}">`;
-        domString +=        `<h2 id="points">${data1.points.total}</h2>`;
+        domString +=        `<h2id="points">${data1.points.total}</h2>`;
         domString +=    `</div>`;
         domString +=    `<div class='col-md-4 col-md-offset-1'>`;
         domString +=        `<img src="${data2.gravatar_url}">`;
-        domString +=        `<h2> id="points">${data2.points.total}</h2>`;
+        domString +=        `<h2id="points">${data2.points.total}</h2>`;
         domString +=    `</div>`
         domString += `<div>`;
         printToDom(domString, "bothPlayers-container");
 };
 
+const winner = (pdata1, pdata2) => {
+    let domString = '';
+    if(pdata1.points.total > pdata2.points.total) {
+        domString += `<div>${pdata1.name} Wins!</div>`;
+    } else {
+        domString += `<div>${pdata2.name} Winnner!!</div>`;
+    }
+    printToDom(domString, "winner-container");
+};
 
+const returnSecondPlayer = (data1, player2) => {
+    const myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', JSONConvert);
+    myRequest.addEventListener('error', allFailed);
+    myRequest.open('GET',`https://teamtreehouse.com/${player2}.json`);
+    myRequest.send();
 
-function successFunction () {
-    const data = JSON.parse(this.responseText);
-    buildDomString(data);
+    function JSONConvert () {
+        const data2 = JSON.parse(this.responseText);
+        winner(data1, data2);
+        buildDomString(data1, data2);
+        }
 };
 
 
-const genericXHRRequest = () => {
-    const myRequest = new HMLHttpRequest();
-    myRequest.addEventListener('load',);
-    myRequest.addEventListener('error',);
-    myRequest.open('GET',);
+function singlePoints () {
+    let player2Name = document.getElementById('player2').value;
+    const data = JSON.parse(this.responseText);
+    returnSecondPlayer(data, player2Name);
+}
+
+
+
+function allFailed () {
+    prompt("Name does Not Exist!");
+}
+
+
+const genericXHRRequest = (urlName, callBack) => {
+    const myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', callBack);
+    myRequest.addEventListener('error', allFailed);
+    myRequest.open('GET',`https://teamtreehouse.com/${urlName}.json`);
     myRequest.send();
 };
 
 const startApplication = () => {
     const button = document.getElementById('button');
     button.addEventListener('click',() => {
-        let
-    })
-}
+        let player1Name = document.getElementById('player1').value;
+        genericXHRRequest(player1Name, singlePoints);
+    });
+};
+
 
 startApplication();
